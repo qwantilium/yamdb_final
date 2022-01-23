49635ce814440ecb383400832912d15483aca259
unknown <qwantilium>
2022-01-23 13:29:38 +0500

    isort fix 2

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -9,9 +9,7 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
-
 from reviews.models import Category, Genre, Review, Title, User
-
 from .filters import TitleFilter
 from .permissions import AuthorModerAdminOrReadOnly, IsAdmin, ReadOnly
 from .serializers import (CategorySerializer, CommentSerializer,
