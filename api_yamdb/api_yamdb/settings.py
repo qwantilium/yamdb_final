707589e224db1010e416bfd2103dbb15b2d3e985
unknown <qwantilium>
2022-02-02 23:40:55 +0500

    push push

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['51.250.28.160', 'localhost']
+ALLOWED_HOSTS = ['51.250.28.160', '127.0.0.1', 'localhost']
 
 # Application definition
 
